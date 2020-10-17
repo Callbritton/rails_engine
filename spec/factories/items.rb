9fefb18856da597db6cbc1f11f9bafad0563c70e
Chris Allbritton <cla.inbox@gmail.com>
2020-10-17 17:11:25 -0400

    Update factories for testing

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :item do
-    name { "MyString" }
-    description { "MyString" }
+    name { Faker::Movies::Lebowski.character }
+    description { Faker::Movies::Lebowski.quote }
     unit_price { 1.5 }
     merchant { nil }
   end
