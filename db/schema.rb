9033cdba02faec7118942cb4116ef86d1944b44f
Chris Allbritton <cla.inbox@gmail.com>
2020-10-17 16:05:28 -0400

    Add credit_card_expiration_date to transactions to complete csv import

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_17_162909) do
+ActiveRecord::Schema.define(version: 2020_10_17_193216) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 2020_10_17_162909) do
     t.string "result"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "credit_card_expiration_date"
     t.index ["invoice_id"], name: "index_transactions_on_invoice_id"
   end
 
