83e79945cef698be39177b2d1cff65e7fb4bdc52
Chris Allbritton <cla.inbox@gmail.com>
2020-10-18 16:09:15 -0400

    Add create test for item

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -56,4 +56,26 @@ describe "Items API" do
     expect(attributes).to have_key(:merchant_id)
     expect(attributes[:merchant_id]).to be_a(Integer)
   end
+
+  it "can create a new item" do
+    merchant = create(:merchant)
+    item_params = ({
+                    name: 'Totally safe slingshot',
+                    description: 'Absolutely wont put your eye out!',
+                    unit_price: 19.99,
+                    merchant_id: merchant.id
+                  })
+
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post "/api/v1/items", headers: headers, params: JSON.generate(item: item_params)
+
+    created_item = Item.last
+
+    expect(response).to be_successful
+    expect(created_item.name).to eq(item_params[:name])
+    expect(created_item.description).to eq(item_params[:description])
+    expect(created_item.merchant_id).to eq(item_params[:merchant_id])
+    expect(created_item.unit_price).to eq(item_params[:unit_price])
+  end
 end
