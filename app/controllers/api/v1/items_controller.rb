5c99139acaf886bf0aa4e52d64ca7e44a362a06d
Chris Allbritton <cla.inbox@gmail.com>
2020-10-18 16:13:32 -0400

    Add update action for items controller

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -12,6 +12,9 @@ class Api::V1::ItemsController < ApplicationController
     render json: ItemSerializer.new(Item.create(item_params))
   end
 
+  def update
+    render json: ItemSerializer.new(Item.update(params[:id], item_params))
+  end
 
   private
 
