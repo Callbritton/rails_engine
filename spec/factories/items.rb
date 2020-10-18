3bc851fec3d66f5b0a9cf0ff368aa1f663ba5ad6
Chris Allbritton <cla.inbox@gmail.com>
2020-10-18 14:43:24 -0400

    Update factories so as to create functional items objects

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :item do
     name { Faker::Movies::Lebowski.character }
     description { Faker::Movies::Lebowski.quote }
-    unit_price { 1.5 }
-    merchant { nil }
+    unit_price { Faker::Commerce.price }
+    merchant
   end
 end
