b185e561b3e48e4f5646fefabd44f1376926d6a6
Chris Allbritton <cla.inbox@gmail.com>
2020-10-20 19:32:08 -0400

    Add test to determine it can get items from a merchant

---
+++ spec/requests/api/v1/merchants/merchant_item_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'Relationships' do
+  it 'can get items for a merchant' do
+    merchant = create(:merchant)
+    5.times {create(:item, merchant: merchant)}
+
+    get "/api/v1/items/#{merchant.id}/items"
+    expect(response).to be_successful
+    expect(response.content_type).to eq("application/json")
+
+    json = JSON.parse(response.body, symbolize_names: true)
+    data = json[:data]
+
+    expect(data.count).to eq(5)
+
+  end
+end
