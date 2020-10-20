a039b7ab93ff2462371e03db6fc59583b777a7ef
Chris Allbritton <cla.inbox@gmail.com>
2020-10-20 19:24:59 -0400

    Add merchants index for items route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :items, only: [:index, :show, :create, :update, :destroy]
       resources :merchants, only: [:index, :show, :create, :update, :destroy]
+
+      namespace :items do
+        get '/:id/merchant', to: 'merchants#index'
+      end
     end
   end
 end
