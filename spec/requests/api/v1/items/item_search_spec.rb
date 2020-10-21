53d079c32d0f08a28bbfd113d4b7850d03a89832
Chris Allbritton <cla.inbox@gmail.com>
2020-10-21 15:35:14 -0400

    Add tests for merchant_search_spec and item_search_spec

---
+++ spec/requests/api/v1/items/item_search_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'Search endpoints' do
+  it 'can find a list of items that contain a fragment, case insensitive' do
+    item = create(:item)
+    merchant = item.merchant
+
+    get "/api/v1/items/#{item.id}/merchants"
+    expect(response).to be_successful
+    expect(response.content_type).to eq("application/json")
+
+    json = JSON.parse(response.body, symbolize_names: true)
+    data = json[:data]
+    attributes = data[:attributes]
+
+    expect(data[:id]).to eq(merchant.id.to_s)
+    expect(attributes[:name]).to eq(merchant.name)
+  end
+end
---
+++ spec/requests/api/v1/merchants/merchant_search_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'Search endpoints' do
+  it 'can find a merchants that contain a fragment, case insensitive' do
+    merchant = Merchant.create(name: "Toy Store")
+    merchant_name = "STORE"
+
+    get "/api/v1/merchants/find?name=#{merchant_name}"
+    expect(response).to be_successful
+    expect(response.content_type).to eq("application/json")
+
+    json = JSON.parse(response.body, symbolize_names: true)
+    data = json[:data]
+    attributes = data[:attributes]
+
+    expect(data[:id]).to eq("#{merchant.id}")
+    expect(data[:type]).to eq('merchant')
+    expect(attributes[:name]).to eq(merchant.name)
+  end
+end
