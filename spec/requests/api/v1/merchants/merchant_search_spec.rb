32262f38be4e11ff28811dfdfa79dbaa227ab000
Chris Allbritton <cla.inbox@gmail.com>
2020-10-21 16:28:03 -0400

    Add test to confirm it can find a list of merchants that is not an exact match to query

--- spec/requests/api/v1/merchants/merchant_search_spec.rb
+++ spec/requests/api/v1/merchants/merchant_search_spec.rb
@@ -18,4 +18,26 @@ RSpec.describe 'Search endpoints' do
     expect(data[:type]).to eq('merchant')
     expect(attributes[:name]).to eq(merchant.name)
   end
+
+  it 'can find a list of merchants that contains a fragment, case insensitive' do
+    merchant = Merchant.create(name: "Toy Store")
+    merchant = Merchant.create(name: "oy StO")
+    merchant = Merchant.create(name: "Bike Shop")
+    merchant = Merchant.create(name: "STORE")
+    merchant = Merchant.create(name: "Tire Rack")
+    merchant = Merchant.create(name: "StOrAgE SHACK")
+
+    attribute = :name
+    value = "sto"
+
+    get "/api/v1/merchants/find_all?#{attribute}=#{value}"
+    expect(response).to be_successful
+    expect(response.content_type).to eq("application/json")
+
+    json = JSON.parse(response.body, symbolize_names: true)
+    data = json[:data]
+    attributes = data[:attributes]
+# Need to find additional ways to test this...
+    expect(json.count).to eq(4)
+  end
 end
