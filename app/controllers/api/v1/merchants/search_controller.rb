3ef163aabee83fd2b185f1709109d1aefb050a03
Chris Allbritton <cla.inbox@gmail.com>
2020-10-21 16:15:03 -0400

    Add show action and attribute and value methods

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -1,4 +1,14 @@
 class Api::V1::Merchants::SearchController < ApplicationController
   def show
+    merchant = Merchant.find_by("#{attribute} ILIKE '%#{value}%'")
+    render json: MerchantSerializer.new(merchant)
+  end
+
+  def attribute
+    request.query_parameters.keys[0]
+  end
+
+  def value
+    request.query_parameters.values[0]
   end
 end
