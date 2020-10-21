5dd2ce4ab105f3d4f648e55f14766e97967c2800
Chris Allbritton <cla.inbox@gmail.com>
2020-10-21 16:14:32 -0400

    Minor refactoring to store attributes and values in variables to match search_controller show action

--- spec/requests/api/v1/merchants/merchant_search_spec.rb
+++ spec/requests/api/v1/merchants/merchant_search_spec.rb
@@ -3,9 +3,10 @@ require 'rails_helper'
 RSpec.describe 'Search endpoints' do
   it 'can find a single merchant that contains a fragment, case insensitive' do
     merchant = Merchant.create(name: "Toy Store")
-    merchant_name = "STORE"
+    attribute = :name
+    value = "TOY ST"
 
-    get "/api/v1/merchants/find?name=#{merchant_name}"
+    get "/api/v1/merchants/find?#{attribute}=#{value}"
     expect(response).to be_successful
     expect(response.content_type).to eq("application/json")
 
