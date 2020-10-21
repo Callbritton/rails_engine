5a9edbdfb5c09a98352f2486cafe2c826f3165c1
Chris Allbritton <cla.inbox@gmail.com>
2020-10-21 15:49:43 -0400

    Update routes to add the search routing for merchant

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,17 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :items, only: [:index, :show, :create, :update, :destroy]
-      resources :merchants, only: [:index, :show, :create, :update, :destroy]
+      namespace :merchants do
+        get '/:id/items', to: 'items#index'
+        get '/find', to: 'search#show'
+      end
 
       namespace :items do
         get '/:id/merchants', to: 'merchants#index'
       end
 
-      namespace :merchants do
-        get '/:id/items', to: 'items#index'
-      end
+      resources :items, only: [:index, :show, :create, :update, :destroy]
+      resources :merchants, only: [:index, :show, :create, :update, :destroy]
     end
   end
 end
